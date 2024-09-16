fa31fe3b2c9f2ae99837d2ec321eb757a07ee249
Harrish Easwar <harisheaswar@gmail.com>
2024-09-16 14:31:39 +0530

    refactor the function extraRunsConcededByYear() and use filter and redeuce methods

--- src/server/2-extra-runs-conceded-by-year.js
+++ src/server/2-extra-runs-conceded-by-year.js
@@ -3,54 +3,33 @@ import fs from "fs";
 const matches=JSON.parse(fs.readFileSync("../data/matches.json","utf-8"));
 const deliveries=JSON.parse(fs.readFileSync("../data/deliveries.json","utf-8"));
 
-// Some teams did not play in the year 2016. They are also included here.
+
 export function extraRunsConcededByYear(matches,deliveries,year=2016){
 
-    const matchIds=[];
-    const resultObj={};
-    //save the match ids in an array where season is 2015. Saving team names as keys in resultObj
-        for(let index=0;index<matches.length;index++){
-           const team1=matches[index]["team1"];
-           const team2=matches[index]["team2"];
-            if(matches[index]["season"]!=undefined && parseInt(matches[index]["season"])===year){
-                matchIds.push(matches[index].id);
-            }
+   const idArr=matches.filter((match)=>{
+    let matchYear=match["season"];
 
-            if(!resultObj.hasOwnProperty(team1)){
-                    resultObj[team1]=0;
-            }
-            if(!resultObj.hasOwnProperty(team2)){
-                resultObj[team2]=0;
-            }
+        if(parseInt(matchYear)===year){
+            return true;
+        }
+   }).map(match=>match["id"]);
 
-        }
-        //Comparing delivery match id and matchesId array(we made in the previous step)
-        for(let index=0;index<deliveries.length;index++){
-            
-           
-            if(deliveries[index]["match_id"]!=undefined && matchIds.includes(deliveries[index]["match_id"])){ 
-                const bowlingTeam=deliveries[index]["bowling_team"];
-                
-                // Pushing extra runs into resultObj
-                if(bowlingTeam!=undefined && resultObj.hasOwnProperty(bowlingTeam)){
-                    const extraRuns=deliveries[index]["extra_runs"];
-                        
-                    if(extraRuns!=undefined){
-                        resultObj[bowlingTeam]+=parseInt(extraRuns);
-                    }
-                }
-            }
-            
-        }
-
-        for(let team in resultObj){
-            if(resultObj[team]===0){
-                resultObj[team]="Did not play IPL that year";
-            }
-        }
+    const resultObj=deliveries.reduce((resObj,delivery)=>{
+        const id=delivery["match_id"];
+        const bowling=delivery["bowling_team"];
         
-        return resultObj;
-    
+        if(idArr.includes(id)){
+
+            if(!resObj.hasOwnProperty(bowling)){
+                resObj[bowling]=0;
+            }
+                resObj[bowling]+=parseInt(delivery["extra_runs"]);
+        }
+        return resObj;
+        
+    },{});
+
+    return resultObj
 }
 
 

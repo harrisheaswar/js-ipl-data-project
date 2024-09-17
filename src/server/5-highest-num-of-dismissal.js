8f6f216e5068c0399f4b10dfb3d93d3a6646d443
Harrish Easwar <harisheaswar@gmail.com>
2024-09-17 17:05:04 +0530

    refactor the function highestNumOfDismissal()

--- src/server/5-highest-num-of-dismissal.js
+++ src/server/5-highest-num-of-dismissal.js
@@ -1,100 +1,84 @@
 import fs from "fs";
 
-const deliveries=JSON.parse(fs.readFileSync("/home/harrish-easwar/js-ipl-data-project/src/data/deliveries.json","utf-8"));
+const deliveries = JSON.parse(
+  fs.readFileSync(
+    "/home/harrish-easwar/js-ipl-data-project/src/data/deliveries.json",
+    "utf-8"
+  )
+);
 
+export function highestNumOfDismissals(deliveries) {
+  let playerListObj = deliveries.reduce((resObj, delivery) => {
+    let playerOut = delivery["player_dismissed"];
+    let bowler = delivery["bowler"];
 
-export function highestNumOfDismissals(deliveries){
-    let playerList={};
-    let resultObj={};
-    // creating an object of players who were dismissed and dismissed by who and number of times dismissed
-    for(let index=0;index<deliveries.length;index++){
+    let wicketKeeperOrFielder = delivery["fielder"];
 
-        const dismissedPlayer=deliveries[index]["player_dismissed"];
-        const dismissedType=deliveries[index]["dismissal_kind"];
-        const bowler=deliveries[index]["bowler"];
-        const wicketKeeperOrFielder=deliveries[index]["fielder"];
+    if (playerOut) {
+      if (!resObj.hasOwnProperty(playerOut)) {
+        resObj[playerOut] = {};
+      }
 
-        //if the main object does not have dismissed player then add them as obj key else it checks type of dimissal and adds player who dismissed. 
-        if(dismissedPlayer!="" && !playerList.hasOwnProperty(dismissedPlayer)){
-            playerList[dismissedPlayer]={};
+      if (bowler && !resObj[playerOut].hasOwnProperty(bowler)) {
+        resObj[playerOut][bowler] = 0;
+      }
+      if (
+        wicketKeeperOrFielder &&
+        !resObj[playerOut].hasOwnProperty(wicketKeeperOrFielder)
+      ) {
+        resObj[playerOut][wicketKeeperOrFielder] = 0;
+      }
 
-        }else if(dismissedPlayer!="" && playerList.hasOwnProperty(dismissedPlayer)){
-
-                //checking based on dismissal type. Stumped is considered for both keeper and bowler
-                if(dismissedType!="" && dismissedType==="stumped"){
-                    //bowler dismissal count increases
-                        if(bowler!=""  && !playerList[dismissedPlayer].hasOwnProperty(bowler)){
-                            playerList[dismissedPlayer][bowler]=1;
-                        }else if(bowler!=""){
-                            playerList[dismissedPlayer][bowler]+=1;
-                        }
-
-                        //wicket keeper dismissal count increases
-                        if(wicketKeeperOrFielder!="" && !playerList[dismissedPlayer].hasOwnProperty(wicketKeeperOrFielder)){
-                            playerList[dismissedPlayer][wicketKeeperOrFielder]=1;
-                        }else{
-                            playerList[dismissedPlayer][wicketKeeperOrFielder]+=1;
-                        }
-                }else if(dismissedType!="" && dismissedType==="run out"){
-                        //checking for only run outs. Fielder gets this dismissal
-                        if(wicketKeeperOrFielder!="" && !playerList[dismissedPlayer].hasOwnProperty(wicketKeeperOrFielder)){
-                            playerList[dismissedPlayer][wicketKeeperOrFielder]=1;
-                        }else{
-                            playerList[dismissedPlayer][wicketKeeperOrFielder]+=1;
-                        }
-                }else{
-                        //checking for only bowler dismissals
-                        if(bowler!=""  && !playerList[dismissedPlayer].hasOwnProperty(bowler)){
-                            playerList[dismissedPlayer][bowler]=1;
-                        }else if(bowler!=""){
-                            playerList[dismissedPlayer][bowler]+=1;
-                        }
-                }
-
-
-
-            }
-
+      if (delivery["dismissal_kind"] === "stumped") {
+        resObj[playerOut][wicketKeeperOrFielder] += 1;
+        resObj[playerOut][bowler] += 1;
+      } else if (delivery["dismissal_kind"] === "run out") {
+        resObj[playerOut][wicketKeeperOrFielder] += 1;
+      } else {
+        resObj[playerOut][bowler] += 1;
+      }
     }
-    //searching the playerList Obj to check the player who was dismissed the most and by which player.
-    let mostDismissed;
-    let dismisserFinal;
-    let numOfTimes=0;
-    for(let dismissedPlayer in playerList){
-        
-        for(let dismisser in playerList[dismissedPlayer]){
-            if(playerList[dismissedPlayer][dismisser]>numOfTimes){
 
-                mostDismissed=dismissedPlayer;
-                dismisserFinal=dismisser;
-                numOfTimes=playerList[dismissedPlayer][dismisser];
-            }
-        }
+    return resObj;
+  }, {});
+
+  let max = 0;
+  let batsman;
+  let bowler;
+
+  for (let playerOut in playerListObj) {
+    for (let dismisser in playerListObj[playerOut]) {
+      if (playerListObj[playerOut][dismisser] > max) {
+        max = playerListObj[playerOut][dismisser];
+        batsman = playerOut;
+        bowler = dismisser;
+      }
     }
-    //saving the results in an object
-    resultObj[mostDismissed]={
-        dismisser: dismisserFinal,
-        no_of_times: numOfTimes
-    };
-    
-    return resultObj;
+  }
+
+  let resultObj = {};
+  resultObj[batsman] = {
+    bowler: bowler,
+    num_of_outs: max,
+  };
+
+  return resultObj;
 }
 
-
-
-
 //write and dump to .json
-function dumpJsonToFile(result){
-    const jsonRes=JSON.stringify(result,null,2);
-    fs.writeFileSync("../public/output/highestNumOfDismissal.json",jsonRes,"utf-8");
-    
+function dumpJsonToFile(result) {
+  const jsonRes = JSON.stringify(result, null, 2);
+  fs.writeFileSync(
+    "../public/output/highestNumOfDismissal.json",
+    jsonRes,
+    "utf-8"
+  );
+}
 
-};
+function runAndDumpRes() {
+  const highestNumOfDismissals1 = highestNumOfDismissals(deliveries);
 
-function runAndDumpRes(){
-    const highestNumOfDismissals1=highestNumOfDismissals(deliveries);
-
-    dumpJsonToFile(highestNumOfDismissals1);
+  dumpJsonToFile(highestNumOfDismissals1);
 }
 
 runAndDumpRes();

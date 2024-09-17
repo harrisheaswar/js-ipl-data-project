466ac0c4a74fc79704f6217fea1c35703db60d73
Harrish Easwar <harisheaswar@gmail.com>
2024-09-17 17:47:52 +0530

    refactor the function bowlerWithBestEcoInSuperOver()

--- src/server/1-bowler-with-best-eco-super-over.js
+++ src/server/1-bowler-with-best-eco-super-over.js
@@ -1,74 +1,72 @@
 import fs from "fs";
 
-const deliveries=JSON.parse(fs.readFileSync("../data/deliveries.json","utf-8"));
+const deliveries = JSON.parse(
+  fs.readFileSync("../data/deliveries.json", "utf-8")
+);
 
+export function bowlerWithBestEcoSuperOver(deliveries) {
+  let bowlersList = deliveries.reduce((resObj, delivery) => {
+    if (delivery["is_super_over"] === "1") {
+      let bowler = delivery["bowler"];
+      let wideRuns = delivery["wide_runs"];
+      let noBallRuns = delivery["noball_runs"];
+      let batsmanRuns = delivery["batsman_runs"];
 
-export function bowlerWithBestEcoSuperOver(deliveries){
+      if (!resObj.hasOwnProperty(bowler)) {
+        resObj[bowler] = {
+          runs: 0,
+          balls: 0,
+        };
+      }
 
-    let bowlerList={};
-    let resultObj={};
+      if (wideRuns && parseInt(wideRuns) > 0) {
+        resObj[bowler].runs += parseInt(wideRuns);
+      } else if (noBallRuns && parseInt(noBallRuns) > 0) {
+        resObj[bowler].runs += parseInt(noBallRuns);
+      } else if (batsmanRuns && parseInt(batsmanRuns) > 0) {
+        resObj[bowler].runs += parseInt(batsmanRuns);
+        resObj[bowler].balls += 1;
+      } else {
+        resObj[bowler].balls += 1;
+      }
+    }
+    return resObj;
+  }, {});
 
-    //saving bowlers(super-over) as keys and storing number of runs conceded and number of balls bowled
-        for(let index=0;index<deliveries.length;index++){
-            let bowler=deliveries[index]["bowler"];
-            //checking if it is a super over and the bowler is not present in list. Else it checks for runs and balls bowled
-            if(deliveries[index]["is_super_over"]==="1" && !bowlerList.hasOwnProperty(bowler)){
-                bowlerList[bowler]={
-                    total_runs:0,
-                    total_balls:0
-                }
-            }else if(deliveries[index]["is_super_over"]==="1"){
-                
-                const wideRuns=parseInt(deliveries[index]["wide_runs"]);
-                const noBallRuns=parseInt(deliveries[index]["noball_runs"]);
-                const batsmanRuns=parseInt(deliveries[index]["batsman_runs"]);
+  let bestBowler;
+  let bestEco = Number.MAX_SAFE_INTEGER;
+  for (let bowler in bowlersList) {
+    let eco = (bowlersList[bowler]["runs"] / bowlersList[bowler]["balls"]) * 6;
 
+    if (eco < bestEco) {
+      bestBowler = bowler;
+      bestEco = eco;
+    }
+  }
 
-                if(wideRuns>0){
-                    bowlerList[bowler]["total_runs"]+=wideRuns;
-                }else if(noBallRuns>0){
-                    bowlerList[bowler]["total_runs"]+=noBallRuns;
-                }else if(batsmanRuns>0){
-                    bowlerList[bowler]["total_runs"]+=batsmanRuns;
-                    bowlerList[bowler]["total_balls"]+=1;
-                }else{
-                    bowlerList[bowler]["total_balls"]+=1;
-                }
-            }
-        }
+  let resultObj = {
+    bowler: bestBowler,
+    economy: bestEco,
+  };
 
-    //calculating the best economy from all the super-over bowlers 
-    let bestBowler;
-    let bestEconomy=Number.MAX_SAFE_INTEGER;
-
-        for(let bowler in bowlerList){
-            //economy is total_run/total_overs OR (total_runs/total_balls)*6
-            let economy=(bowlerList[bowler]["total_runs"]/bowlerList[bowler]["total_balls"])*6;
-            if(economy<bestEconomy){
-                bestBowler=bowler;
-                bestEconomy=economy;
-                
-            }
-        }
-        //saving the results of the best super over bowler by economy in the below object
-    resultObj[bestBowler]=bestEconomy;
-    
-    return resultObj;
-
+  return resultObj;
 }
 
 //write and dump to .json
-function dumpJSONToFile(bowlerWithBestEcoSuperOver1){
-    const jsonRes=JSON.stringify(bowlerWithBestEcoSuperOver1,null,2);
+function dumpJSONToFile(bowlerWithBestEcoSuperOver1) {
+  const jsonRes = JSON.stringify(bowlerWithBestEcoSuperOver1, null, 2);
 
-    fs.writeFileSync("../public/output/bowlerWithBestEcoSuperOver.json",jsonRes,"utf-8");
+  fs.writeFileSync(
+    "../public/output/bowlerWithBestEcoSuperOver.json",
+    jsonRes,
+    "utf-8"
+  );
 }
 
+function runAndDump() {
+  const bowlerWithBestEcoSuperOver1 = bowlerWithBestEcoSuperOver(deliveries);
 
-function runAndDump(){
-    const bowlerWithBestEcoSuperOver1=bowlerWithBestEcoSuperOver(deliveries);
-
-    dumpJSONToFile(bowlerWithBestEcoSuperOver1);
+  dumpJSONToFile(bowlerWithBestEcoSuperOver1);
 }
 
 runAndDump();
